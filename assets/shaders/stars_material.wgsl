c1485aa3b00fba22c7149033f46c62c02241da63
regalk <jsalazarcaviedes30@gmail.com>
2024-11-16 21:54:47 -0500

    Stars and rockets

--- assets/shaders/stars_material.wgsl
+++ assets/shaders/stars_material.wgsl
@@ -4,21 +4,20 @@
 @group(2) @binding(1) var<uniform> time: f32;
 
 fn random2(p: vec2<f32>) -> f32 {
-    let dot_product = dot(p, vec2<f32>(12.9898, 78.233)); // This is the random function
-    return fract(sin(dot_product) * 43758.5453);  // Generates pseudo-random values
+    let dot_product = dot(p, vec2<f32>(12.9898, 78.233));
+    return fract(sin(dot_product) * 43758.5453);
 }
 
 @fragment
 fn fragment(vertex: VertexOutput) -> @location(0) vec4<f32> {
     let uv = vertex.position.xy;
 
-    let motion_offset = vec2<f32>(time * 0.1, 0.0);
+    let motion_offset = vec2<f32>(0.01, 0.0);
 
-    let scaled_uv = uv * 0.1; // Further decrease the scale for much larger stars
+    let scaled_uv = uv * 0.2; // Further decrease the scale for much larger stars
 
-    let brightness = step(0.995, random2(scaled_uv + motion_offset)); // Increase threshold to 0.995 to drastically reduce stars
+    let brightness = step(0.995, random2(scaled_uv + motion_offset));
+    let density_factor = step(0.05, fract(scaled_uv.x) * fract(scaled_uv.y)); 
 
-    let density_factor = step(0.05, fract(scaled_uv.x) * fract(scaled_uv.y)); // Lower density factor for fewer stars
-
-    return material_color * vec4<f32>(brightness * density_factor, brightness * density_factor, brightness * density_factor, 1.0);
+    return material_color * vec4<f32>(brightness * density_factor, brightness * density_factor, brightness * density_factor, 0.8);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use bevy::{
     prelude::*,
     reflect::TypePath,
+      core_pipeline::{
+        bloom::BloomSettings,
+        tonemapping::Tonemapping,
+    },
     render::render_resource::{
             AsBindGroup, ShaderRef, 
     },
@@ -25,9 +29,24 @@ impl Material2d for MovingPatternMaterial {
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins,  Material2dPlugin::<MovingPatternMaterial>::default()))
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, add_sun))
         .add_systems(Update, update_position)
         .run();
+}
+
+fn add_sun(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials:  ResMut<Assets<ColorMaterial>>,
+) {
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(Circle::new(50.0))).into(),
+        material: materials.add(Color::srgb(7.0, 7.0, 0.0)),
+        transform: Transform::from_xyz(0.0, 0.0, 3.0),
+        ..default()
+    });
+
 }
 
 fn setup(
@@ -39,7 +58,17 @@ fn setup(
     let window = window.single();
     let resolution = Vec2::new(window.width(), window.height());
 
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                hdr: true,
+                ..default()
+            },
+            tonemapping: Tonemapping::TonyMcMapface, 
+            ..default()
+        },
+        BloomSettings::default(),
+    ));
     commands.spawn(MaterialMesh2dBundle {
         mesh: meshes.add(Mesh::from(Rectangle::from_size(resolution))).into(),
         material: materials.add(MovingPatternMaterial {
